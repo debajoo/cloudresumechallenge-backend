57ed1ccac4a9d45d1194914ac48b9009e126ff88
Adesunmisola Adebajo <adesunmisolaadebajo@Adesunmisolas-MacBook-Air.local>
2022-05-13 19:45:55 +0100

    code edit

--- Lambda/app.py
+++ Lambda/app.py
@@ -21,7 +21,7 @@ def lambda_handler(event, context):
     )
 
     # Format dynamodb response into variable
-    responseBody = json.dumps(int(float(ddbResponse["Attributes"]["visitor_count"])))
+    responseBody = json.dumps({"count":int(float(ddbResponse["Attributes"]["visitor_count"]))})
     # Create api response object
     apiResponse = {
         "isBase64Encoded": False,
